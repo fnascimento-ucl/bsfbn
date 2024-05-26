3fd44aeca37d435f9b42335cf7c97411d8fd1713
fnascimento-ucl <159649062+fnascimento-ucl@users.noreply.github.com>
2024-05-26 19:43:31 +0100

    Add files via upload

--- R/launch_app.R
+++ R/launch_app.R
@@ -1,5 +1,11 @@
-# In R/launch_app.R
+#' Launch the Shiny Application
+#'
+#' This function launches the Shiny application.
+#' @export
 launch_app <- function() {
   app_dir <- system.file("shiny", package = "bsfbn")
-  shiny::runApp(app_dir)
+  if (app_dir == "") {
+    stop("Could not find example directory. Try re-installing `bsfbn`.", call. = FALSE)
+  }
+  shiny::runApp(app_dir, display.mode = "normal")
 }
